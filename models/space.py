665e558f45113c5384273785016bd202f6faddf6
Peter Emu <emu.kimani@strathmore.edu>
2026-02-03 11:42:02 +0300

    Add to_dict() method to Space model for JSON serialization

--- models/space.py
+++ models/space.py
@@ -13,3 +13,16 @@ class Space(db.Model):
     price = db.Column(db.Float, nullable=False)
     location = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def to_dict(self):
+        """
+        Convert Space object to dictionary for JSON serialization
+        """
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'price': self.price,
+            'location': self.location,
+            'created_at': self.created_at.isoformat() if self.created_at else None
+        }
