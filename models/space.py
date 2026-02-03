ab93a7f57560c9c55d30134053bd32c4a695ecfa
Peter Emu <emu.kimani@strathmore.edu>
2026-02-03 11:41:39 +0300

    Add description, price, location, and created_at fields to Space model

--- models/space.py
+++ models/space.py
@@ -9,3 +9,7 @@ class Space(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
