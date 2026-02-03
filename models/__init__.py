d973ce183558183f13336fc29dbdb480d5cfad4f
Abdimalik <binabdimalikranger@gmail.com>
2026-02-03 08:51:36 +0300

    project setup

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,18 @@
-# models/__init__.py
+"""
+models/__init__.py - Models Package Initialization
+
+This file makes the models directory a Python package and exports
+the model classes for easy importing elsewhere in the application.
+
+Usage:
+    from models import Space
+    
+Note: The main Space model is defined in app.py for this project.
+This separate models/space.py file is an alternative structure.
+"""
+
+# Import the Space model from the space module
 from .space import Space
 
+# Define what gets exported when using "from models import *"
 __all__ = ['Space']
